6c9a5ad113353d36452db5dbd5840ead2d450773
Cyanmin <tomohiro20030220@gmail.com>
2025-01-06 22:37:22 +0900

    Discord-Bot初期設定

---
+++ src/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+});
+
+// Botトークンでログイン
+client.login(process.env.DISCORD_TOKEN);
